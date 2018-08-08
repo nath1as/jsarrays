6a0cf58b90fb31f9ae3e04fd36b6bc8f9d168fe3
nathias <n@th1.as>
2018-08-08 15:59:20 +0200

    last index of

---
+++ lastIndexOf.js
@@ -0,0 +1,16 @@
+function indexOf(arr, val) {
+  var index = -1;
+  var i;
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      index = i;
+    }
+  }
+
+  console.log(index);
+}
+
+indexOf([1, 2, 3, 3], 3);         // 2
+indexOf([1, 2, 3], 4);            // -1
+
