5a3683c3010896a725bbba6f6ef330bd0d45f03e
nathias <n@th1.as>
2018-08-08 19:19:44 +0200

    nth element

---
+++ nthElementOf.js
@@ -0,0 +1,9 @@
+function nthElementOf(arr, index) {
+  console.log(arr[index]);
+}
+
+var digits = [4, 8, 15, 16, 23, 42];
+nthElementOf(digits, 3);   // returns 16
+nthElementOf(digits, 8);   // what does this return?
+nthElementOf(digits, -1);  // what does this return?
+
