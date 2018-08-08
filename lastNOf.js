23c8874dc9bc34b57fcc1df0ae519ecfc102968e
nathias <n@th1.as>
2018-08-08 19:30:21 +0200

    improved last n of array

--- lastNOf.js
+++ lastNOf.js
@@ -1,12 +1,23 @@
-function lastNOf(arr, count) {
-  var newArr = [];
-  var i;
-  for (i = count; i < arr.length; i++) {
-    newArr.push(arr[i]);
+// function lastNOf(arr, count) {
+//   var newArr = [];
+//   var i;
+//   for (i = count; i < arr.length; i++) {
+//     newArr.push(arr[i]);
+//   }
+//   console.log(newArr);
+// }
+
+function lastNOf(arr, length) {
+  var index = arr.length - length;
+
+  if (index < 0) {
+    index = 0;
   }
-  console.log(newArr);
+
+  return arr.slice(index);
 }
 
 var digits = [4, 8, 15, 16, 23, 42];
 lastNOf(digits, 3);    // returns [16, 23, 42]
+lastNOf(digits, 15);    // returns [16, 23, 42]
 
